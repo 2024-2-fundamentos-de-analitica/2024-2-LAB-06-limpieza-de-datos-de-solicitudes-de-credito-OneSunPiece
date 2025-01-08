31be20335b713ede09ad6d1ff56dee0bdfde8a4d
codeinpieces <68757620+codeinpieces@users.noreply.github.com>
2025-01-08 18:52:06 -0500

    D-0-N-3

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,6 +1,7 @@
 """
 Escriba el codigo que ejecute la accion solicitada en la pregunta.
 """
+import pandas as pd
 
 
 def pregunta_01():
@@ -13,3 +14,28 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep=";", index_col=0)
+    df = df.copy()
+
+    columnas=["sexo", "tipo_de_emprendimiento", "idea_negocio", "monto_del_credito", "línea_credito"]
+
+    for col in columnas:
+        df[col] = df[col].str.lower().str.strip()
+        df[col] = df[col].str.replace("_", " ").str.replace("-", " ")
+        df[col] = df[col].str.replace(",", "").str.replace("$", "")
+        df[col] = df[col].str.replace(".00", "") 
+        df[col] = df[col].str.strip()
+
+    df["barrio"]=df["barrio"].str.lower()
+    df["barrio"]=df["barrio"].str.replace("_", " ")
+    df["barrio"]=df["barrio"].str.replace("-", " ")
+    df["comuna_ciudadano"] = df["comuna_ciudadano"].astype(int)
+    df["monto_del_credito"].astype(int)
+    df['monto_del_credito'] = df['monto_del_credito'].astype(float)
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce").combine_first(pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce"))
+    
+ 
+    df = df.drop_duplicates()
+    df = df.dropna()
+
+    df.to_csv("files/output/solicitudes_de_credito.csv", sep=";", index=False) 
